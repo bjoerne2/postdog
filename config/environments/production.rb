dd1f4d7dd9910290e3076a0efe1132a60edef043
Björn Weinbrenner <info@bjoerne.com>
2017-07-11 22:32:59 +0200

    Configure SMTP mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "messaging-example_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAILER_SMTP_ADDRESS'],
+    port:                 ENV['MAILER_SMTP_PORT'],
+    authentication:       :plain,
+    user_name:            ENV['MAILER_SMTP_USER_NAME'],
+    password:             ENV['MAILER_SMTP_PASSWORD'],
+    enable_starttls_auto: false,
+    ssl:                  true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
