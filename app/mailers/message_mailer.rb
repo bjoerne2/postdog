165cb83c27c61414eaf289f8187acaf0350eb83f
Björn Weinbrenner <info@bjoerne.com>
2017-07-12 14:39:36 +0200

    Simple add '-' and token before @ of from address; email tags aren't well supported yet

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,7 +5,7 @@ class MessageMailer < Mailboxer::MessageMailer
     receipts = @message.receipts.select { |receipt| receipt.receiver == @receiver && receipt.mailbox_type == 'inbox' }
     raise "Unexpected number of receipts (= #{receipts.count}) for receiver #{@receiver.email}" if receipts.count != 1
     receipt = receipts.first
-    headers[:from] = Mailboxer.default_from.sub /@|\+.*@/, "+#{receipt.token}@"
+    headers[:from] = Mailboxer.default_from.sub '@', "-#{receipt.token}@"
     super(headers, &block)
   end
 end
