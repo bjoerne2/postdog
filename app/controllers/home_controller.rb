2e3026f3a2c030de3ea3cfa5b9041bec076bba6a
Björn Weinbrenner <info@bjoerne.com>
2017-07-16 09:36:31 +0200

    Handle unmatched routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,16 @@
 class HomeController < ApplicationController
   def index
+    flash.keep
     if user_signed_in?
       redirect_to mailbox_inbox_path
     else
       flash[:warning] = "This is a demo application and your data will be deleted after 1 day." if 'true' == ENV['DEMO_MODE']
       redirect_to new_user_session_path
     end
+  end
+
+  def unmatched_route
+    flash[:error] = 'Page not found'
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
   get "trash" => "mailbox#trash", as: :mailbox_trash
   delete "empty_trash" => "mailbox#empty_trash", as: :empty_mailbox_trash
 
-  resources :conversations do
+  resources :conversations, only: [:show, :new, :create] do
     member do
       post :reply
       post :trash
       post :untrash
     end
   end
+
+  get '*unmatched_route', to: 'home#unmatched_route'
 end
